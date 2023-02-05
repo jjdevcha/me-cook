e12cdd4b92b43d2cdaed8ae24fcf71da5333b77a
Jung In <jungincha923@gmail.com>
2023-02-05 22:35:23 +1000

    New feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -139,6 +139,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log("Welcome to the application");
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -147,6 +151,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
